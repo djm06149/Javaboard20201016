05cb1d899a77a1121f6209f48fe14b7afd4b2a46
djm06149 <djm06149@gmail.com>
2020-10-16 18:59:03 +0900

    202010161900

--- src/Java_board.java
+++ src/Java_board.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
 
 public class Java_board {
 
 	public static void main(String[] args) {
-		System.out.println("Start");
+		Scanner sc = new Scanner(System.in);
+
+		String title = "";
+		String body = "";
+		while (true) {
+			System.out.print("명령어 입력 : ");
+			String str = sc.next();
+
+			if (str.equals("exit")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+			if (str.equals("add")) {
+				System.out.println("게시물 제목을 출력해주세요 : ");
+				title = sc.next();
+				System.out.println("게시물 내용을 출력해주세요 : ");
+				body = sc.next();
+				System.out.println("게시물이 등록되었습니다.");
+			}
+			if (str.equals("list")) {
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
+			}
+		}
 
 	}
 
