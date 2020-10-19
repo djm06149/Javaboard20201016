424ad9dc7edb90a67ffce7ae9311455f66187f16
djm06149 <djm06149@gmail.com>
2020-10-19 12:52:14 +0900

    dddd

--- src/Java_board.java
+++ src/Java_board.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Java_board {
@@ -5,8 +6,11 @@ public class Java_board {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String title = "";
-		String body = "";
+		ArrayList<String> titles = new ArrayList<>();
+		ArrayList<String> bodies = new ArrayList<>();
+
+		int size = 0;
+
 		while (true) {
 			System.out.print("명령어 입력: ");
 			String str = sc.next();
@@ -16,18 +20,40 @@ public class Java_board {
 				break;
 			}
 			if (str.equals("add")) {
-				System.out.println("게시물 제목 입력 : ");
-				title = sc.next();
-				System.out.println("게시물 내용 입력 : ");
-				body = sc.next();
+				System.out.print("게시물 제목 입력 : ");
+				titles.add(sc.next());
+				System.out.print("게시물 내용 입력 : ");
+				bodies.add(sc.next());
 				System.out.println("게시물이 등록되었습니다.");
+				size++;
 			}
 			if (str.equals("list")) {
-				System.out.println("제목 : " + title);
-				System.out.println("내용 : " + body);
+				for (int i = 0; i < size; i++) {
+					System.out.println("제목 : " + titles.get(i));
+					System.out.println("내용 : " + bodies.get(i));
+					System.out.println("======================");
+
+				}
+			}
+			if (str.equals("update")) {
+				System.out.print("수정할 게시물 선택 : ");
+				String targetTitle = sc.next();			
+				
+				for(int i = 0; i < titles.size(); i++) {
+					String title = titles.get(i);
+					if (titles.equals(targetTitle)) {
+						System.out.print("게시물 제목 입력 : ");
+						String newTitle = sc.next();
+						System.out.print("게시물 내용 입력 : ");
+						String newBody = sc.next();
+						
+						titles.set(i, newTitle);
+						bodies.set(i, newBody);
+						break;
+					}
+				}
+				
 			}
 		}
-
 	}
-
 }
