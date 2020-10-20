c286be9ae817937e865301ef28f98a8bc7e11c1e
djm06149 <djm06149@gmail.com>
2020-10-20 12:47:17 +0900

    aaaa

--- src/Java_board.java
+++ src/Java_board.java
@@ -6,10 +6,11 @@ public class Java_board {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		ArrayList<Integer> ids = new ArrayList<>();
 		ArrayList<String> titles = new ArrayList<>();
 		ArrayList<String> bodies = new ArrayList<>();
 
-		int size = 0;
+		int no = 1;
 
 		while (true) {
 			System.out.print("명령어 입력: ");
@@ -20,15 +21,18 @@ public class Java_board {
 				break;
 			}
 			if (str.equals("add")) {
+				ids.add(no);
+				no++;
 				System.out.print("게시물 제목 입력 : ");
 				titles.add(sc.next());
 				System.out.print("게시물 내용 입력 : ");
 				bodies.add(sc.next());
 				System.out.println("게시물이 등록되었습니다.");
-				size++;
+
 			}
 			if (str.equals("list")) {
-				for (int i = 0; i < size; i++) {
+				for (int i = 0; i < ids.size(); i++) {
+					System.out.println("번호 : " + ids.get(i));
 					System.out.println("제목 : " + titles.get(i));
 					System.out.println("내용 : " + bodies.get(i));
 					System.out.println("======================");
@@ -37,22 +41,46 @@ public class Java_board {
 			}
 			if (str.equals("update")) {
 				System.out.print("수정할 게시물 선택 : ");
-				String targetTitle = sc.next();			
-				
-				for(int i = 0; i < titles.size(); i++) {
-					String title = titles.get(i);
-					if (titles.equals(targetTitle)) {
+				int targetId = sc.nextInt();
+
+				for (int i = 0; i < ids.size(); i++) {
+					int id = ids.get(i);
+					if (id == targetId) {
 						System.out.print("게시물 제목 입력 : ");
 						String newTitle = sc.next();
 						System.out.print("게시물 내용 입력 : ");
 						String newBody = sc.next();
-						
+
 						titles.set(i, newTitle);
 						bodies.set(i, newBody);
 						break;
+
 					}
 				}
-				
+			}
+			if (str.equals("delete")) {
+				System.out.print("몇번 게시물을 지우시겠습니까 : ");
+				int targetId = sc.nextInt();
+				int existFlag = 2; // 1. 있음 2. 없음
+
+				for (int i = 0; i < ids.size(); i++) {
+					int id = ids.get(i);
+					if (id == targetId) {
+						existFlag = 1;
+						ids.remove(i);
+						titles.remove(i);
+						bodies.remove(i);
+						break;
+					}
+				}
+
+				if (existFlag == 2) {
+
+					System.out.println("게시물이 존재하지 않습니다.");
+				} else {
+					
+					System.out.println(targetId + "번 게시물이 삭제되었습니다.");
+				}
 			}
 		}
 	}
